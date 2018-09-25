166b46cf42463a15aed5cd5e10daed47074343c4
benjamin-summers <summerstyler2@gmail.com>
2018-09-25 19:33:44 -0400

    added delete items functionality

--- src/assets/components/TodoList.js
+++ src/assets/components/TodoList.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import TodoItems from './TodoItems';
+import '../css/TodoList.css';
 
 export default class TodoList extends Component {
     constructor(props) {
@@ -9,6 +10,7 @@ export default class TodoList extends Component {
         };
 
         this.addItem = this.addItem.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
     }
 
     addItem(e) {
@@ -32,6 +34,16 @@ export default class TodoList extends Component {
         e.preventDefault();
     }
 
+    deleteItem(key) {
+        var filteredItems = this.state.items.filter(function (item) {
+            return (item.key !== key);
+        });
+
+        this.setState({
+            items: filteredItems
+        });
+    }
+
     render() {
         return (
             <div className="todoListMain">
@@ -41,7 +53,7 @@ export default class TodoList extends Component {
                         <button type="submit">add</button>
                     </form>
                 </div>
-                <TodoItems entries={this.state.items}/>
+                <TodoItems entries={this.state.items} delete={this.deleteItem}/>
             </div>
         )
     }
